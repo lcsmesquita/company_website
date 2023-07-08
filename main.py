5c20decb06c770a35af527dc0738a77a2fb1d35c
Lucas Mesquita <lucas.mesquita1110@gmail.com>
2023-07-08 15:29:17 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# Here i will need to develop a company website
+# It's a task that i got delivered by the teacher of the course i am taking.
+
+import streamlit as st
+import pandas
+
+st.title("The best company")
+#First, a need a title that is written "The best company"
+
+#Second, i need a subtitle telling something about the company
+
+#then i need another title that shows "Our team"
+
+#After that, i need to show in 3 columns, the name of de person, then, 
+#its function in the company, and its picture. All of that is in the data.csv and images.
